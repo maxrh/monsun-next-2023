30c753ffdca14b3f9ab1387831467168e9e3ed7c
Max Hargreave <max@monsun.dk>
2023-06-05 19:12:39 +0200

    crash crash

--- app/components/SquaresGrid.js
+++ app/components/SquaresGrid.js
@@ -1,19 +1,27 @@
 "use client"
 
 import { useEffect, useState, useMemo, useRef } from 'react'
-import { motion, AnimatePresence } from 'framer-motion'
+import { motion } from 'framer-motion'
 import useRandomHslColor from '../hooks/useRandomHslColor'
 import styles from './SquaresGrid.scss'
 
 export default function SquaresGrid({ size, gap, className }) {
 
+    const hueRanges = [
+        [0, 30],    [30, 60],   [60, 90],   [90, 120], 
+        [120, 150], [150, 180], [180, 210], [210, 240], 
+        [240, 270], [270, 300], [300, 330], [330, 360]
+    ];
+
     const [numberOfSquares, setNumberOfSquares] = useState(0)
     const [numColumns, setNumColumns] = useState(0)
-    const [isLoading, setIsLoading] = useState() // New state
-    const ref = useRef(null);
+    const [numRows, setNumRows] = useState(0)
 
-    const squareSize = size || 5 // Use prop or default
-    const gridGap = gap || 20 // Use prop or default
+    const [isLoading, setIsLoading] = useState(false)  
+
+    const ref = useRef(null);
+    const squareSize = size || 5 
+    const gridGap = gap || 20 
 
     useEffect(() => {
         const handleResize = () => {
@@ -26,12 +34,12 @@ export default function SquaresGrid({ size, gap, className }) {
                 let numRows = Math.floor(height / (squareSize + gridGap))
         
                 if (numCols % 2 === 0) { numCols-- }
-        
+                
+                setNumRows(numRows)
                 setNumColumns(numCols)
                 const totalSquares = numCols * numRows
                 setNumberOfSquares(totalSquares)
             }
-
             setTimeout(() => setIsLoading(false), 500) // Set loading state to false
         }
 
@@ -43,61 +51,51 @@ export default function SquaresGrid({ size, gap, className }) {
         }
     }, [])
 
-    // set animation on mouse enter
+    
+    
 
-
+    const row = numRows;
+    const column = numColumns;
 
     const squares = useMemo(() => {
         return Array.from({ length: numberOfSquares }, (_, i) => {
-            const row = Math.floor(i / numColumns);
-            const column = i % numColumns;
-
             return (
-                <motion.div key={i} i={i}
+                <motion.div
+                    key={i}
+                    i={i}
                     className="square"
+                    initial={{
+                        opacity: 1,
+                        backgroundColor: 'rgba(10, 13, 18, 1)' 
+                    }}
+                    animate={{
+                        opacity: 1,
+                        backgroundColor: ['rgb(19, 68, 153)', 'rgba(10, 13, 18, 1)', 'rgb(237, 39, 8)'],
+                    }} 
+                    transition={{ 
+                        ease: "easeInOut",
+                        duration: 15,
+                        restDelta: 0.001
+                    }}
                     style={{ 
                         width: `${squareSize}px`, 
                         height: `${squareSize}px`,
                         gridRow: row + 1,
                         gridColumn: (row % 2 ? column * 2 : column * 2 + 1) + 1,
-                    }}
-                    initial={{ backgroundColor: 'rgba(10, 13, 18, 1)'}}
-                    animate={{ opacity: 1, backgroundColor: useRandomHslColor()}}
-                    exit={{ opacity: 0 }}
-                    transition={{ 
-                        ease: "linear",
-                        duration: 2,
-                        delay:  (Math.random() * 2 + 0.1) - 0.1,
-                        restDelta: 0.001
-                    }}
-                    onMouseEnter={() => {
-                        console.log(i, 'mouse enter')
-
-                        // ad opacity to hovered square
-
-                        const square = document.querySelector(`.square-grid .square:nth-child(${i + 1})`)
-                        square.style.opacity = .1
-                        square.style.transition = 'opacity .5s ease-in-out'
-                        
-
-                    }}
+                    }}                
                 />
             );
         });
+
     }, [numberOfSquares, squareSize]);
+
 
     return (
         <>
-
             { isLoading ? (
-                
-                <motion.div 
+                <div 
                     ref={ref} 
                     className={`square-grid ${className}`} 
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    exit={{ opacity: 0 }}
-                    transition={{ duration: .25 }}
                     style={{
                         display: 'flex',
                         justifyContent: 'center',
@@ -106,16 +104,14 @@ export default function SquaresGrid({ size, gap, className }) {
                     }}
                 >
                     <span className="bulma-loader-mixin"></span>
-                </motion.div>
+                </div>
 
             ) : (
                 <motion.div 
                     ref={ref} 
                     className={`square-grid ${className}`} 
                     initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    exit={{ opacity: 0 }}
-                    transition={{ duration: .25 }}
+                    animate={{ opacity: 1 }} 
                     style={{
                         display: 'grid',
                         gridTemplateColumns: `repeat(${numColumns * 2}, ${squareSize}px)`,
@@ -124,13 +120,11 @@ export default function SquaresGrid({ size, gap, className }) {
                         gridRowGap: `${gridGap}px`,
                         perspective: '1000px',
                     }}
+                    
                 >
-
                     {squares}
-
                 </motion.div>
             )}
-
        </>
     );
 }
--- app/hooks/useRandomHslColor.js
+++ app/hooks/useRandomHslColor.js
@@ -1,20 +1,12 @@
-const hueRanges = [
-    [0, 30],    [30, 60],   [60, 90],   [90, 120], 
-    [120, 150], [150, 180], [180, 210], [210, 240], 
-    [240, 270], [270, 300], [300, 330], [330, 360]
-];
-
-const randomHueRange = hueRanges[Math.floor(Math.random() * hueRanges.length)];
-
-const useRandomHslColor = () => {
+const useRandomHslColor = (hueRange) => {
     const getRandomNumber = (min, max) => Math.round(Math.random() * (max - min) + min);
     const { hue, saturation, lightness } = {
-        hue: getRandomNumber(...randomHueRange),
-        saturation: getRandomNumber(0, 100),
-        lightness: getRandomNumber(0, 100),
+      hue: getRandomNumber(...hueRange),
+      saturation: getRandomNumber(0, 100),
+      lightness: getRandomNumber(0, 100),
     };
-
+  
     return `hsl(${hue}, ${saturation}%, ${lightness}%)`;
-};
+  };
 
 export default useRandomHslColor;
